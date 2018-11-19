230221a6a4c21da2aec1db636f34461e4f983c80
seancsutton1 <seancsutton1@gmail.com>
2018-11-19 16:59:18 -0600

    data base working

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+const {
+  db,
+  auth
+} = require('./database.js')
+const hostname = 'localhost';
+const port = 8080;
+const app = express();
+
+app.use(express.static('dist/alfred'))
+
+app.get('/', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('connected');
+});
+
+
+app.listen(port, hostname, () => {
+  // connect to the DB
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
