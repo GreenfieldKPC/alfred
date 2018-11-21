39d23920bf91b6d7351a83a24aa5b5a3c20b4ad6
Kevin <kqd4ng@gmail.com>
2018-11-20 19:57:37 -0600

    got the login to get the username and redirect to the dashboard

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-log-in',
+//   templateUrl: './log-in.component.html',
+//   styleUrls: ['./log-in.component.css']
+// })
+// export class LogInComponent implements OnInit {
+//   loginForm: FormGroup;
+//   constructor(private formBuilder: FormBuilder) { }
+
+//   ngOnInit() {
+//     this.loginForm = this.formBuilder.group({
+//       username: [''],
+//       password: ['']
+//     })
+//   }
+//   onLogin() {
+//     console.log(this.loginForm.value);
+//   }
+
+// }
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent implements OnInit {
-  loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+export class LogInComponent {
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      username: [''],
-      password: ['']
-    })
-  }
-  onLogin() {
-    console.log(this.loginForm.value);
+  username = '';
+  password = '';
+
+  constructor(private router: Router) {
   }
 
+  tryLogin() {
+    
+      if (this.username && this.password){
+        this.router.navigateByUrl('/dashboard');
+
+      }
+    
+  }
 }
