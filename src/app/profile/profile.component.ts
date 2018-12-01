2192fef80552772c30f7f94d2b0f91624cbf2085
Kevin <kqd4ng@gmail.com>
2018-11-30 20:01:14 -0600

    profile has user info render on page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { JobService } from '../job.service'
 // import { toArray } from 'rxjs/operators';
-
+import { DashboardService } from '../dashboard.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,23 +10,37 @@ import { JobService } from '../job.service'
 export class ProfileComponent implements OnInit {
   categoryLists = ['House Hold', 'Lawn Care', 'Pet Care'];
   selectedCategory: string;
-
+  hold:any;
+  info:any;
   public takenCount: Number;
   public postedCount: Number;
 
-  constructor(private _jobService: JobService) {
+  constructor(
+    private _jobService: JobService,
+    private dashboardService: DashboardService
+    ) {
     this.takenCount = 0;
     this.postedCount = 0;
-   }
-
+  }
+  userInfo() {
+    this.dashboardService.getUser()
+      .subscribe((data) => {
+        this.info = data;
+        console.log(this.info);
+      })
+  }
   ngOnInit() {
 
-
+    this.userInfo();
     this._jobService.getUserJobsTaken().then(data => {
-     console.log(data)
+      console.log(data)
+      this.hold = data;
+      this.takenCount = this.hold.length;
     });
     this._jobService.getUserJobsPosted().then(data => {
-  console.log(data)
+      console.log(data)
+      this.hold = data;
+      this.postedCount = this.hold.length;
     });
   }
 
